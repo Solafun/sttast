3000600d2f8356e759138ee220739a6eecca6e13
Sola.fun <71711340+Solafun@users.noreply.github.com>
2026-03-07 15:23:57 +0300

    Delete api directory

--- api/action.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({
-    message: 'Bot action endpoint',
-    status: 'ok'
-  });
-};
--- api/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({
-    name: 'Threads Reply Bot API',
-    version: '1.0',
-    endpoints: ['/api/action']
-  });
-};
